ecd4f7ab114faeaad3923efe41e739950e9e5ea2
grabiz <lythedan1411@gmail.com>
2018-01-21 10:54:19 +0700

    init project

---
+++ android/app/configs/firebase.js
@@ -0,0 +1,12 @@
+<script src="https://www.gstatic.com/firebasejs/4.9.0/firebase.js"></script>
+<script>
+  var config = {
+    apiKey: "AIzaSyCXnltOB6KTqDqZDSZdFQ63n--0cCEpVC8",
+    authDomain: "reactdemo-a0e6e.firebaseapp.com",
+    databaseURL: "https://reactdemo-a0e6e.firebaseio.com",
+    projectId: "reactdemo-a0e6e",
+    storageBucket: "reactdemo-a0e6e.appspot.com",
+    messagingSenderId: "835010991787"
+  };
+  firebase.initializeApp(config);
+</script>
